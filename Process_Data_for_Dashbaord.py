56c66760d503db7a3f8d1adbf04c15f96aab9090
mattyrobin8 <mattyrobin8@gmail.com>
2021-04-15 18:19:50 -0400

    Renamed Keep Columns object, first attempt at adding fourth csv to import loop

--- Process_Data_for_Dashbaord.py
+++ Process_Data_for_Dashbaord.py
@@ -4,13 +4,14 @@ import pandas as pd
 
 def import_data(file_location):
     '''Import the data and append to one dataframe'''
-    df  = pd.DataFrame(columns=columns_to_keep)
+    df  = pd.DataFrame(columns=keep_cols)
     for datafile in file_location:
         data = pd.read_csv(datafile)
-        data = data[columns_to_keep]
+        if datafile == r"C:\Users\matty\OneDrive\Politics\Mayor Felicia\Data\COBRA-2009-2019.csv":
+            data = data.rename(columns = {'Report Date': keep_cols[0], 'UCR Literal': keep_cols[1], 'Neighborhood': keep_cols[2], 'Latitude': keep_cols[3], 'Longitude': keep_cols[4]}, inplace = True)
+        data = data[keep_cols]
         df = df.append(data, ignore_index = True)
     return df		
-
 
 #####Define objects#####
 
@@ -18,11 +19,11 @@ def import_data(file_location):
 file1 = r"C:\Users\matty\OneDrive\Politics\Mayor Felicia\Data\COBRA-2021.csv"
 file2 = r"C:\Users\matty\OneDrive\Politics\Mayor Felicia\Data\COBRA-2020(NEW RMS 9-30 12-31).csv"
 file3 = r"C:\Users\matty\OneDrive\Politics\Mayor Felicia\Data\COBRA-2020-OldRMS-09292020.csv"
-#file4 = r"C:\Users\matty\OneDrive\Politics\Mayor Felicia\Data\COBRA-2009-2019.csv"
-file_list = [file1, file2, file3]
+file4 = r"C:\Users\matty\OneDrive\Politics\Mayor Felicia\Data\COBRA-2009-2019.csv"
+file_list = [file1, file2, file3, file4]
 
 #Which columns to keep
-columns_to_keep = ['rpt_date','UC2_Literal','neighborhood','lat','long']
+keep_cols = ['rpt_date','UC2_Literal','neighborhood','lat','long']
 
 
 #####Run functions#####
