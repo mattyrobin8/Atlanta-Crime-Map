7f09df335d6d737058f40bff295e0e8bd4095939
mattyrobin8 <mattyrobin8@gmail.com>
2021-05-05 17:23:32 -0400

    Add merge and export in order that I can create an incomplete dataset. Then will complete later

--- Process_Data_for_Dashbaord.py
+++ Process_Data_for_Dashbaord.py
@@ -43,12 +43,17 @@ def create_geo_file(df):
     for row in range(1,len(df.index)):
         geo_object = df[row-1:row].apply(get_geodata, axis=1, geolocator=geolocator, lat_field='lat', lon_field='long')
         for index, value in geo_object.items():
-            temp_df = pd.DataFrame([[value['lat'], value['lon'], value['address']['postcode']]], columns = match_cols)
+            try:
+                temp_df = pd.DataFrame([[value['lat'], value['lon'], value['address']['postcode']]], columns = match_cols)
+                temp_df['lat'] = pd.to_numeric(temp_df['lat'])
+                temp_df['long'] = pd.to_numeric(temp_df['long'])
+            except:
+                pass
             match_df = match_df.append(temp_df, ignore_index = True)
             print(len(match_df.index))
-    match_df['lat'] = pd.to_numeric(match_df['lat'])
-    match_df['long'] = pd.to_numeric(match_df['long'])
-    return match_df
+            match_geo_df = pd.merge(df, match_df, how="inner", left_index=True, right_index=True, sort=True, suffixes=("_orig", "_match"), copy=True, validate=None)
+            match_geo_df.to_csv(r'C:\Users\matty\OneDrive\Politics\Mayor Felicia\Data\crime_with_zips.csv', index = False)
+    return match_geo_df
 
 def merge_export_df(df1,df2):
     '''Merge the original and match dataframes then export'''
@@ -92,12 +97,12 @@ def main():
     #Extract zip codes and matching information from geodata
     #match_df = create_match_file(geodata)
 
-    #Retrieve GeoData
-    match_df = create_geo_file(crime_df)
-    print(match_df)
-
     #Merge the dataframes the export
-    merge_export_df(crime_df, match_df)
+    #merge_export_df(crime_df, match_df)
+    
+    #Retrieve GeoData
+    match_geo_df = create_geo_file(crime_df)
+    print(match_geo_df)
 
 if __name__ == '__main__':
     start_time = time.time()
