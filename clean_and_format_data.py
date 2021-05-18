8fe00c5be1a72220a42e69d73c14ae5e77317698
mattyrobin8 <mattyrobin8@gmail.com>
2021-05-18 18:51:18 -0400

    Update clean_and_format_data.py
    Format queries

--- clean_and_format_data.py
+++ clean_and_format_data.py
@@ -37,20 +37,20 @@ def main():
 	crime_df = import_data(file_location)
 
 	#Create ATL crime dataframe
-	crime_query = """
-			select  	strftime('%Y', rpt_date) as year
-						,strftime('%m', rpt_date) as month
-						,strftime('%Y%m', rpt_date) as year_month
-						,zipcode
-						,Crime
-						,count(*) as total_crime
-			from 		crime_df
-			group by 	Crime
-						,zipcode
-						,strftime('%Y', rpt_date)
-						,strftime('%m', rpt_date)
-					 	,strftime('%Y%m', rpt_date)
-		"""
+	crime_query = 	"""
+					select  	strftime('%Y', rpt_date) as year
+								,strftime('%m', rpt_date) as month
+								,strftime('%Y%m', rpt_date) as year_month
+								,zipcode
+								,Crime
+								,count(*) as total_crime
+					from 		crime_df
+					group by 	Crime
+								,zipcode
+								,strftime('%Y', rpt_date)
+								,strftime('%m', rpt_date)
+								,strftime('%Y%m', rpt_date)
+					"""
 	atlcrime_df = ps.sqldf(crime_query)
 
 	#Create ATL population dataframe
@@ -59,18 +59,19 @@ def main():
 
 	#Join ATL crime and population dataframes
 	crimepop_query = """
-			select  	crime.year
-						,year_month
-						,zipcode
-						,Crime
-						,total_crime
-						,population
-			from 		atlcrime_df crime
-			join		atlpop_df pop
-			on			crime.year = pop.year
-			where		zipcode not in ('None')
-			order by 	year_month
-		"""
+					select  	crime.year
+								,month
+								,year_month
+								,zipcode
+								,Crime
+								,total_crime
+								,population
+					from 		atlcrime_df crime
+					join		atlpop_df pop
+					on			crime.year = pop.year
+					where		zipcode not in ('None')
+					order by 	year_month
+					"""
 	atlcrimepop_df = ps.sqldf(crimepop_query)
 	atlcrimepop_df['crimes_per_100K'] = (atlcrimepop_df['total_crime'] / atlcrimepop_df['population']) * 100000
 	print(atlcrimepop_df)
