2f6bb0d5fda02111320f41829f1baf5350718310
mattyrobin8 <mattyrobin8@gmail.com>
2021-06-07 20:02:41 -0400

    Add current month script
    Remove print from original script and create new script that only looks at current monthly data

--- month_over_month.py
+++ month_over_month.py
@@ -28,7 +28,7 @@ def import_data(file_location):
 
 #List files
 file_location = r"C:\Users\matty\OneDrive\Politics\Mayor Felicia\Out\crime_with_zips.csv"
-export_file = r"C:\Users\matty\OneDrive\Politics\Mayor Felicia\Out\crime_for_tableau.csv"
+export_file = r"C:\Users\matty\OneDrive\Politics\Mayor Felicia\Out\crime_for_tableau_month.csv"
 
 #Create ATL population dataframe
 data = [['2018',459600],['2019',470500],['2020',478200],['2021',(478200-470500) + 478200]]
@@ -59,86 +59,18 @@ crime_query = 	"""
 							,Crime
 				"""
 
-#Pull denominator for extrapolation of 2021 data
-numerator_2021_query = """
-				select  	year
-							,Crime
-							,sum(total_crime) as total_crime
-				from 		atlcrime_df
-				where		zipcode not in ('None')
-				and			year = 2020
-				group by	year
-							,Crime
-				order by 	year
-				"""
-
-#Pull numerator for extrapolation of 2021 data
-denominator_2021_query = """
-				select  	year
-							,Crime
-							,sum(total_crime) as total_crime
-				from 		atlcrime_df
-				where		zipcode not in ('None')
-				and			month in ('01','02','03','04','05')
-				and			year = 2020
-				group by	year
-							,Crime
-				order by 	year
-							,Crime
-				"""
-
-#Create index for 2021 extrapolation
-index_2021_query = """
-				select		num.Crime
-							,avg(cast(num.total_crime as float) / cast(dem.total_crime as float)) as crime_index
-				from 		numerator_2021_df num
-				join		denominator_2021_df dem
-				on			num.year = dem.year
-				and			num.Crime = dem.Crime
-				group by	num.Crime
-				"""
-
-#Apply 2021 crime index
-crime_2021_query = """
-				select 	year
-						,zipcode
-						,crime.Crime
-						,cast(round(sum(total_crime) * crime_index,0) as int) as total_crime
-				from atlcrime_df crime
-				join index_2021_df ind
-				on crime.Crime = ind.Crime
-				where year = 2021
-				group by year
-						,zipcode
-						,crime.Crime
-				"""
-
-#Pull 2020 crime data
-crime_2020_query = """
-				select 	year
-						,zipcode
-						,Crime
-						,sum(total_crime) as total_crime
-				from atlcrime_df
-				where year not in (2021)
-				group by year
-						,zipcode
-						,Crime
-				"""
-
 #Join ATL crime and population dataframes
 crimepop_query = """
 				select  	crime.year
 							,zipcode
-							,Crime
 							,population
 							,sum(total_crime) as total_crime
 							,(sum(cast(total_crime as float)) / cast(population as float)) * 100000 as crimes_per_100k
-				from 		crime_aggregated_df crime
+				from 		atlcrime_df crime
 				join		atlpop_df pop
 				on			crime.year = pop.year
+                where       month in ('01','02','03','04','05')
 				group by 	zipcode
-							,Crime
 							,crime.year
 							,population
 				"""
@@ -155,31 +87,15 @@ def main():
 	#Run ATL crime query
 	atlcrime_df = ps.sqldf(crime_query)
 
-	#Run 2021 numerator
-	numerator_2021_df = ps.sqldf(numerator_2021_query)
-
-	#Run 2021 denominator
-	denominator_2021_df = ps.sqldf(denominator_2021_query)
-
-	#Run 2021 crime index query 
-	index_2021_df = ps.sqldf(index_2021_query)
-
-	#Run 2021 crime index 
-	crime_2021_df = ps.sqldf(crime_2021_query)
-	
-	#Run 2020 Crime query
-	crime_2020_df = ps.sqldf(crime_2020_query)
-
-	#Append 2021 extrapolated to 2020 actuals
-	crime_aggregated_df = crime_2020_df.append(crime_2021_df, ignore_index=True)
-
 	#Run ATL population query
 	atlcrimepop_df = ps.sqldf(crimepop_query)
 
 	#Transform the data for easy pct change calculation
-	actuals_df = pd.pivot_table(data=atlcrimepop_df, index=['Crime','zipcode'], columns=['year'], values=['total_crime'])
+	#actuals_df = pd.pivot_table(data=atlcrimepop_df, index=['Crime','zipcode'], columns=['year'], values=['total_crime'])
+	actuals_df = pd.pivot_table(data=atlcrimepop_df, index=['zipcode'], columns=['year'], values=['total_crime'])
 	actuals_df = actuals_df.reset_index()
-	actuals_df.columns = ['crime','zipcode','total_crime_2018','total_crime_2019','total_crime_2020','total_crime_2021']
+	#actuals_df.columns = ['crime','zipcode','total_crime_2018','total_crime_2019','total_crime_2020','total_crime_2021']
+	actuals_df.columns = ['zipcode','total_crime_2018','total_crime_2019','total_crime_2020','total_crime_2021']
 	percent_df = actuals_df.iloc[:, 2:].pct_change(axis='columns')
 
 	#Merge and do percent change
@@ -189,18 +105,13 @@ def main():
 	blarg.to_csv(export_file, index=False)
 
 	honk = """
-					select  	crime.year
-								,crime
+					select  	year
 								,population
 								,sum(total_crime) as total_crime
 								,(sum(cast(total_crime as float)) / cast(population as float)) * 100000 as crimes_per_100k
-					from 		crime_aggregated_df crime
-					join		atlpop_df pop
-					on			crime.year = pop.year
-					group by	crime
-								,crime.year
-					order by 	crime
-								,crime.year
+					from 		atlcrimepop_df
+					group by	year
+					order by 	year
 					"""
 	print(ps.sqldf(honk))
 
